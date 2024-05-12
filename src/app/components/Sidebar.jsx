3efffdeac22bb9c0bde8b48ebcbac0be369523f4
adroliecharrian <adroliecharrian@gmail.com>
2024-05-12 13:26:09 +0200

    cambios sidebar aniamcion

--- src/app/components/Sidebar.jsx
+++ src/app/components/Sidebar.jsx
@@ -14,10 +14,10 @@ export default function Sidebar({}) {
     <div
       className={`${
         open ? "w-72" : "w-20"
-      } duration-300 h-screen bg-sidebar-light relative p-5 pt-6`}
+      } duration-100 h-screen bg-sidebar-light relative p-5 pt-6`}
     >
       <Image
-        src="/control-light.png"
+        src="/sidebar/control-light.png"
         width={28}
         height={28}
         className={`absolute cursor-pointer -right-3 top-9 w-6 h-100 bg-white rounded-full border-2 border-sidebar-light ${
@@ -28,10 +28,11 @@ export default function Sidebar({}) {
       />
       <div className="flex gap-x-4 items-center">
         <Image
-          src="/logo.png"
+          src="/sidebar/logo.png"
           width={40}
           height={40}
           className={`cursor-pointer duration-500 ${open && "rotate-[360deg]"}`}
+          alt=""
         />
         <h1
           className={`text-white origin-left font-medium text-xl duration-300 ${
@@ -44,8 +45,8 @@ export default function Sidebar({}) {
       <ul className="pt-6">
         {Menus.map((menu, index) => (
           <li key={index} className={`text-white text-sm flex items-center gap-x-4 cursor-pointer p-2 hover:bg-light-white rounded-md mt-3`}>
-            <Image src={`/${menu.src}.png`} width={30} height={30} />
-            <span className={`${!open && 'hidden'} origin-left duration-200`}>{menu.title}</span>
+            <Image src={`/sidebar/${menu.src}.png`} width={40} height={40} alt=""/>
+            <span className={`${!open && 'hidden'} origin-left duration-300`}>{menu.title}</span>
           </li>
         ))}
       </ul>
