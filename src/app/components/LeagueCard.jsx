3e0a5e1443ef9f9c8ac2131c6d9ca42934299bf5
adroliecharrian <adroliecharrian@gmail.com>
2024-05-15 20:08:30 +0200

    avanzando ligas

---
+++ src/app/components/LeagueCard.jsx
@@ -0,0 +1,20 @@
+"use client";
+import Image from "next/image";
+import "./sidebar.css";
+
+export default function LeagueCard() {
+  return (
+    <div className="w-100 rounded-lg overflow-hidden shadow-lg text-center bg-background-light">
+      <Image
+        src="/sidebar/logo.png"
+        width={300}
+        height={300}
+        className={`cursor-pointer duration-500 w-full p-3`}
+        alt=""
+      />
+      <div className="py-4">
+        <div className="font-bold text-xl mb-2">The Coldest Sunset</div>
+      </div>
+    </div>
+  );
+}
--- src/app/hub/leagues/page.jsx
+++ src/app/hub/leagues/page.jsx
@@ -1,7 +1,20 @@
 import React from "react";
+import LeagueCard from "@/app/components/LeagueCard";
 
 const Leagues = () => {
-  return <p></p>;
+  return (
+    <div className="h-100 w-100">
+      <div className="mb-3">
+        <button className="bg-sidebar-light hover:bg-sidebar-light2 text-white font-bold py-2 px-4 rounded">Create League</button>
+      </div>
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-3">
+        <LeagueCard />
+        <LeagueCard />
+        <LeagueCard />
+        <LeagueCard />
+      </div>
+    </div>
+  );
 };
 
 export default Leagues;
