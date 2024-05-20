9315b1fe56d42b831a380745e571cf53b0f1e56f
unknown <carloswl1998@gmail.com>
2024-05-20 16:02:18 +0200

    added screen sizes

--- src/app/components/homeComponent.jsx
+++ src/app/components/homeComponent.jsx
@@ -2,6 +2,7 @@
 import "./home.css"
 import React, { useRef, useEffect } from "react";
 import { Merienda } from "next/font/google";
+import Link from "next/link";
 
 const merienda = Merienda({ subsets: ["latin"] });
 
@@ -32,47 +33,47 @@ const HomeComponent = (props) => {
         <div className="w-full">
             {/* LEFT SIDE */}
             <div ref={left}
-                className="bg-sidebar-dark2 text-white h-screen w-full absolute leading-none side z-10">
-                <nav className="bg-transparent w-5/6 text-xl flex items-center justify-between">
+                className="bg-sidebar-dark2 text-white h-screen w-screen absolute leading-none side z-10">
+                <nav className="bg-transparent w-full text-xl flex items-center justify-between px-6 pt-4 sm:text-3xl sm:pt-12">
                     <ul className="flex">
                         <li className="mr-6">
-                            <a href="#">HOME</a>
+                            <Link href="/">HOME</Link>
                         </li>
                         <li className="mr-6">
                             <a href="#">About</a>
                         </li>
                     </ul>
-                    <div className="flex gap-2">
-                        {props.session ? <img className="h-8 w-8 rounded-full" /> :
+                    <div className="">
+                        {props.session ? <img className="h-9 w-9 sm:h-14 sm:w-14 border rounded-full" /> :
                         <button className="">
-                            <a className="text-gray-400" href="">LogIn</a>
+                            <Link className="" href="/login">LogIn</Link>
                         </button>}
                     </div>
                 </nav>
-                <h1 ref={h1Ref} className="title">
+                <h1 ref={h1Ref} className="title text-5xl sm:text-7xl md:text-8xl xl:px-32">
                 Ultimate League & Team{" "}
                     <span className={`${merienda.className} text-sidebar-light2`}>Manager</span>
                 </h1>
             </div>
             {/* RIGHT SIDE */}
-            <div id="right-side" className="bg-sidebar-light h-screen w-screen absolute leading-none side">
-                <nav className="bg-transparent w-5/6 text-xl flex items-center justify-between">
-                    <ul class="flex">
-                        <li class="mr-6">
-                            <a href="#">HOME</a>
+            <div className="bg-sidebar-light h-screen w-screen absolute leading-none side">
+                <nav className="bg-transparent w-full text-xl flex items-center justify-between px-6 pt-4 sm:text-3xl sm:pt-12">
+                    <ul className="flex">
+                        <li className="mr-6">
+                            <Link href="/">HOME</Link>
                         </li>
-                        <li class="mr-6">
+                        <li className="mr-6">
                             <a href="#">About</a>
                         </li>
                     </ul>
                     <div className="flex gap-2">
-                        {props.session ? <img className="h-8 w-8 rounded-full" /> :
+                        {props.session ? <img className="h-9 w-9 border-black sm:h-14 sm:w-14 border rounded-full" /> :
                         <button className="">
-                            <a className="" href="">LogIn</a>
+                            <Link className="" href="/login">LogIn</Link>
                         </button>}
                     </div>
                 </nav>
-                <h1 className="title">
+                <h1 className="title text-5xl sm:text-7xl md:text-8xl xl:px-32">
                 Ultimate League & Team{" "}
                     <span className={`${merienda.className} text-sidebar-dark`}>Creator</span>
                 </h1>
