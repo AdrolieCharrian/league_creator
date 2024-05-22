b4a4d167693fc067c09f75505a90370747124197
adroliecharrian <adroliecharrian@gmail.com>
2024-05-22 20:14:55 +0200

    aspecto visual landing navbar

--- src/app/hub/layout.jsx
+++ src/app/hub/layout.jsx
@@ -3,11 +3,13 @@ import "../globals.css";
 import {PrismaClient} from "@prisma/client";
 import {cookies} from "next/headers";
 import jwt from "jsonwebtoken";
+import { auth } from "auth";
 
-export default function RootLayout({children}) {
+export default async function RootLayout({children}) {
+  const session = await auth();
   return (
     <div className="flex">
-      <Sidebar />
+      <Sidebar session={session} image={session.user.image} name={session.user.name}/>
       <div className="p-7 text-2x1 font-semibold flex-1 h-screen overflow-y-scroll">
         {children}
       </div>
