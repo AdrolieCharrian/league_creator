db73408233608a717fd20da838e4e6327df704b7
adroliecharrian <adroliecharrian@gmail.com>
2024-07-07 23:30:15 +0200

    borrar liga arreglado, borrar equipo en config hecho

--- src/app/hub/leagues/[id]/admin/page.jsx
+++ src/app/hub/leagues/[id]/admin/page.jsx
@@ -3,11 +3,13 @@ import React from "react";
 import LeaguePlayers from "@/app/components/LeaguePlayers";
 import LeagueSports from "@/app/components/LeagueSports";
 import LeagueInfoForm from "@/app/components/LeagueInfoForm";
+import LeagueTeams from "@/app/components/LeagueTeams";
 
 const Admin = ({ params: { id } }) => {
   return (
     <div className="h-full w-full pt-3">
       <LeagueInfoForm leagueId={id} />
+      <LeagueTeams leagueId={id}/>
       <div className="flex flex-col md:flex-row mt-4">
         <LeagueSports leagueId={id} />
         <LeaguePlayers leagueId={id} />
